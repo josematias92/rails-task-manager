8c16bf3d8accfbcb7cd87a96baa7d59e9a88c431
Jose Matias <josematias92@gmail.com>
2020-06-09 16:18:00 -0500

    Listo el update

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   # Get all tasks
   get 'tasks', to: 'tasks#index'
   # Get just one specific task
-    # Create a new task
+  # Create a new task
   get 'tasks/new', to: 'tasks#new'
 
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 
+  post 'tasks', to: 'tasks#create'
+  #Update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
 end
