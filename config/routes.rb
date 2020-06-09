cea86040f8f31d630961186275cdf2e54b3a79f5
Jose Matias <josematias92@gmail.com>
2020-06-09 15:35:21 -0500

    Display All and Individual

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Get all tasks
+  get 'tasks', to: 'tasks#index'
+  # Get just one specific task
+    # Create a new task
+  get 'tasks/new', to: 'tasks#new'
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
 end
