15c19d742195d86a6a3094d71aaaa54749e9eb9e
Jose Matias <josematias92@gmail.com>
2020-06-09 16:26:21 -0500

    Funciona perfecto

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   #Update a task
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
   patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete'
 end
